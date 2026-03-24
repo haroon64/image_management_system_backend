80bd729e1df13bbc44394d4a74aaab714b33cd35
haroon64 <haroonsiddique97@gmail.com>
2026-03-24 17:29:38 +0500

    images model created and migrated to the database

--- images/models.py
+++ images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
