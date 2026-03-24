a18eacd1ba23c656b3414c8ca567345a76c61cef
haroon64 <haroonsiddique97@gmail.com>
2026-03-25 00:13:46 +0500

    Apis for image upload delete and get all Created

--- images/urls.py
+++ images/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 app_name = 'images'
 
 urlpatterns = [
+    path('', views.ImageListCreateAPIView.as_view(), name='image-list-create'),
+    path('<int:pk>/', views.ImageDeleteAPIView.as_view(), name='image-delete'),
 
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- images/views.py
+++ images/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Image
+from .serializers import ImageSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+
+class ImageListCreateAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser) 
+
+    def get(self, request):
+        images = Image.objects.all()
+        serializer = ImageSerializer(images, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Image uploaded successfully",
+                "data": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ImageDeleteAPIView(APIView):
+
+    def delete(self, request, pk):
+        image = get_object_or_404(Image, pk=pk)
+        image.delete()
+        return Response({
+            "message": "Image deleted successfully"
+        }, status=status.HTTP_204_NO_CONTENT)
