546db50e6a2810e3404075dbe71dcaaaff53a4c9
haroon64 <haroonsiddique97@gmail.com>
2026-03-25 15:53:46 +0500

    integrated register and login api

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/images/', include('images.urls')),
+    path('api/users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    # Allow anyone to hit this endpoint to create an account
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        print(request.data )
+        serializer = RegisterSerializer(data=request.data['user'])
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User created successfully"}, 
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
